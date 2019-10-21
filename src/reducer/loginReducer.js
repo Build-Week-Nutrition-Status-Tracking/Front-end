2453773aabde8e2a26aef126406f79523b58b8f3
Uzias Rivera <uziasrivera@gmail.com>
2019-10-21 13:22:42 -0700

    fleshed out login reducer, commented out for now

--- src/reducer/loginReducer.js
+++ src/reducer/loginReducer.js
@@ -1,57 +1,58 @@
-import {LOGIN_START, LOGIN_FAIL, LOGIN_SUCCESS, LOGOUT, SIGN_IN_START, SIGN_IN_SUCCESS, SIGN_IN_FAIL} from ''
+// import {LOGIN_START, LOGIN_FAIL, LOGIN_SUCCESS, LOGOUT, SIGN_UP_START, SIGN_UP_SUCCESS, SIGN_UP_FAIL} from ''
 
-export const initialState = () =>{
-   return { 
-       error: '',
-       isFetching: false,
-    }
-}
+// export const initialState = () =>{
+//    return { 
+//        error: '',
+//        isFetching: false,
+//     }
+// }
 
-const reducer = (state=initialState, action)=>{
-    switch (action.type){
-        case LOGIN_START:
-            return {
-                ...state,
-                isFetching: true,
-                error:''
-            }
-        case LOGIN_SUCCESS:
-            return {
-                ...state,
-                isFetching: false,
-                error:''
-            }
-        case LOGIN_FAIL:
-            return {
-                ...state,
-                isFetching: false,
-                error:''
-            }
-        case LOGOUT:
-            return {
-                ...state,
-                isFetching: false,
-                error:''
-            }
-        case SIGN_IN_START:
-            return {
-                ...state,
-                isFetching: true,
-                error:''
-            }
-        case SIGN_IN_SUCCESS:
-            return {
-                ...state,
-                isFetching: false,
-                error:''
-            }
-        case SIGN_IN_FAIL:
-            return {
-                ...state,
-                isFetching: false,
-                error:''
-            }
-        default:
-            return state;
-    }
-}
+// const reducer = (state=initialState, action)=>{
+//     switch (action.type){
+//         case LOGIN_START:
+//             return {
+//                 ...state,
+//                 isFetching: true,
+//                 error:''
+//             }
+//         case LOGIN_SUCCESS:
+//             return {
+//                 ...state,
+//                 isFetching: false,
+//                 error:''
+//             }
+//         case LOGIN_FAIL:
+//             return {
+//                 ...state,
+//                 isFetching: false,
+//                 error: action.payload
+//             }
+//         case LOGOUT:
+//             return {
+//                 ...state,
+//                 isFetching: false,
+//                 error:''
+//             }
+//         case SIGN_UP_START:
+//             return {
+//                 ...state,
+//                 isFetching: true,
+//                 error:''
+//             }
+//         case SIGN_UP_SUCCESS:
+//             return {
+//                 ...state,
+//                 isFetching: false,
+//                 error:''
+//             }
+//         case SIGN_UP_FAIL:
+//             return {
+//                 ...state,
+//                 isFetching: false,
+//                 error: action.payload
+//             }
+//         default:
+//             return state;
+//     }
+// }
+
