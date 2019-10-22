77973f0d9c22c79103eb1e880d063edbde42ea03
Uzias Rivera <uziasrivera@gmail.com>
2019-10-21 20:55:19 -0700

    sign in and sign up working

--- src/actions/index.js
+++ src/actions/index.js
@@ -13,25 +13,27 @@ export const registerUser = (user) => dispatch =>{
     console.log('register action')
     dispatch({type:SIGN_UP_START});
     axiosWithAuth()
-    .post('users',user)
+    .post('register',user)
     .then(res=>{dispatch({type:SIGN_UP_SUCCESS, payload: res})
         console.log(res)
-        localStorage.setItem('token',res.data.id)
         })
     .catch(err=>{console.log(err.response)
         dispatch({type:SIGN_UP_FAIL, payload: err})})
 }
 
-export const userLogin = () => dispatch =>{
+export const userLogin = (user) => dispatch =>{
     dispatch({type:LOGIN_START});
     console.log('login action')
-    axios
-    .get('http://localhost:5000')
+    axiosWithAuth()
+    .post('login',user)
     .then(res=>{
         dispatch({type:LOGIN_SUCCESS, payload: res})
         console.log(res)
+        localStorage.setItem('token',res.data.token)
         })
-    .catch(err=>dispatch({type:LOGIN_FAIL, payload: err}))
+    .catch(err=>{
+        console.log(err.response)
+        dispatch({type:LOGIN_FAIL, payload: err})})
 }
 
 export const userLogout = () => dispatch =>{
