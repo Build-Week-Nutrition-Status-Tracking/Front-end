7957e98e2d3c3fa5c7b2accf19b45fa0a83d6443
LN <ln@Ls-Macbook-Pro.local>
2019-10-22 10:55:12 -0700

    flesh out community page

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import logo from "./logo.svg";
 import "./App.css";
 import Signin from "./components/SignIn";
 import SignUp from "./components/SignUp";
+import Community from "./components/Community";
 import { Link, Route } from "react-router-dom";
-
 
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
       <Link to="/signin">Sign In</Link>
       <Route exact path="/signin" component={Signin}></Route>
       <Route path="/signup" component={SignUp} />
+      <Community />
     </div>
   );
 }
