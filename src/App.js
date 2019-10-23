552ef0b182c2b35149f67bd5bb560cb9448eee3a
Uzias Rivera <uziasrivera@gmail.com>
2019-10-22 20:42:34 -0700

    completed user page functionality

--- src/App.js
+++ src/App.js
@@ -3,16 +3,12 @@ import "./App.css";
 
 import Signin from "./components/SignIn";
 import SignUp from "./components/SignUp";
-
-<<<<<<< HEAD
-import CommunityTesting from "./components/CommunityTesting";
-=======
 import Community from "./components/Community";
 import { Link, Route } from "react-router-dom";
->>>>>>> dd997b5470b92926b4f9ab122350d8573059a002
 import Child from "./components/Child";
 import Communities from "./components/Communities";
 import Countries from "./components/Countries";
+import Users from './components/Users'
 
 function App() {
   return (
@@ -25,19 +21,11 @@ function App() {
       <Link to="/child">ChildPage</Link>
       <Route exact path="/signin" component={Signin}></Route>
       <Route path="/signup" component={SignUp} />
-<<<<<<< HEAD
-
-      <Route exact path= '/community' component={CommunityTesting}></Route>
-      <Route path='/child' component={Child} />
-      <Route path='/homepage' component={Countries}/>
-      <Route path='/country/:id' component={Communities}/>
-
-=======
       <Route exact path="/community" component={Community}></Route>
       <Route path="/child" component={Child} />
       <Route path="/homepage" component={Countries} />
       <Route path="/country/:id" component={Communities} />
->>>>>>> dd997b5470b92926b4f9ab122350d8573059a002
+      <Route path='/users' component={Users}/>
     </div>
   );
 }
